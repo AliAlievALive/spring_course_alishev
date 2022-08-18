236ef5c75f4bd115145745784628be95c3c0f293
almir <madarat87@gmail.com>
2022-08-18 13:55:30 +0300

    Authentication 2 AuthProviderImpl with deprecated PasswordEncoder

--- src/main/java/com/almir/springcourse/securityapp/config/SecurityConfig.java
+++ src/main/java/com/almir/springcourse/securityapp/config/SecurityConfig.java
@@ -1,27 +1,25 @@
 package com.almir.springcourse.securityapp.config;
 
-import com.almir.springcourse.securityapp.security.AuthProviderImpl;
+import com.almir.springcourse.securityapp.services.PersonDetailsService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
     }
 
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .antMatcher("/**")
-//                .authorizeRequests(authorize -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                .build();
-//    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
