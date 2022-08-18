e779fe923e21eb8426717e9b075f231c4b81b46b
almir <madarat87@gmail.com>
2022-08-18 13:51:32 +0300

    Authentication AuthProviderImpl with deprecated impl

--- src/main/java/com/almir/springcourse/securityapp/config/SecurityConfig.java
+++ src/main/java/com/almir/springcourse/securityapp/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.almir.springcourse.securityapp.config;
 
+import com.almir.springcourse.securityapp.security.AuthProviderImpl;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @AllArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthProviderImpl authProvider;
 
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authProvider);
+    }
 
 //    @Bean
 //    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
