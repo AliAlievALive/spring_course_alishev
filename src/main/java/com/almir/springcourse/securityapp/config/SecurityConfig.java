a2e69e39ae15a5e1946f73a1e54db02ff4afa386
almir <madarat87@gmail.com>
2022-08-21 18:14:16 +0300

    PreAuthorize annotaion

--- src/main/java/com/almir/springcourse/securityapp/config/SecurityConfig.java
+++ src/main/java/com/almir/springcourse/securityapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @AllArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
--- src/main/java/com/almir/springcourse/securityapp/controllers/HelloController.java
+++ src/main/java/com/almir/springcourse/securityapp/controllers/HelloController.java
@@ -1,13 +1,18 @@
 package com.almir.springcourse.securityapp.controllers;
 
 import com.almir.springcourse.securityapp.security.PersonDetails;
+import com.almir.springcourse.securityapp.services.AdminService;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class HelloController {
+    private final AdminService adminService;
+
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
@@ -24,6 +29,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
